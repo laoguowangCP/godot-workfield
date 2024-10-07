07d0bb4442e6655690d35abec1ca31f8708a5f42
Peng Cheng <18019722385@163.com>
2024-10-08 00:00:34 +0800

    update

--- node_behavior/instantiate_signal/instantiate_signal.gd
+++ node_behavior/instantiate_signal/instantiate_signal.gd
@@ -3,10 +3,13 @@ extends Node
 
 var s : PackedScene = preload("res://node_behavior/instantiate_signal/to_be_instantiated.tscn")
 
+
+func _init():
+    pass
+
 func _ready():
     var x = s.instantiate()
     var x_array = []
     x_array.append(x)
     var y = x_array[0].duplicate(1)
     self.add_child(y)
-
